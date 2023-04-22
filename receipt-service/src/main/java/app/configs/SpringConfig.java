ac4031d0123751a798bc33487be688ff823d42fd
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-04-22 13:19:24 +0300

    add configs

---
+++ receipt-service/src/main/java/app/configs/SpringConfig.java
@@ -0,0 +1,56 @@
+package app.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "app.components")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+    @Bean
+    public ViewResolver jspViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/pages/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        final InputStream file = getClass().getClassLoader().getResourceAsStream("database.properties");
+        final Properties properties = new Properties();
+        try {
+            properties.load(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        dataSource.setDriverClassName(properties.getProperty("driver.name"));
+        dataSource.setUrl(properties.getProperty("db.host"));
+        dataSource.setUsername(properties.getProperty("db.user"));
+        dataSource.setPassword(properties.getProperty("db.password"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+}
+
