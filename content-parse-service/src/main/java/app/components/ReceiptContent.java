254d36b8eb2b3a8ae2c6aa636fcf152e289818ca
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-04-27 00:17:19 +0300

    Add parse service

---
+++ content-parse-service/src/main/java/app/components/ReceiptContent.java
@@ -0,0 +1,16 @@
+package app.components;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ReceiptContent {
+    private String dateTime;
+    private BigDecimal amount;
+
+    public ReceiptContent(String dateTime, BigDecimal amount) {
+        this.dateTime = dateTime;
+        this.amount = amount;
+    }
+}
