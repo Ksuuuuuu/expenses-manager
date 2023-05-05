f47c953f3aa50bffba7a48e394015ffe8584f111
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-05 16:52:06 +0300

    change jdbc to namedjdbc

--- user-service/src/main/java/app/configs/SpringConfig.java
+++ user-service/src/main/java/app/configs/SpringConfig.java
@@ -3,7 +3,7 @@ package app.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -48,9 +48,8 @@ public class SpringConfig implements WebMvcConfigurer {
     }
 
     @Bean
-    public JdbcTemplate jdbcTemplate() {
-        return new JdbcTemplate(dataSource());
+    public NamedParameterJdbcTemplate jdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
     }
-
 }
 
