fda8dbdc961a1a8ac1846a262118b96b9266b578
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-17 17:35:47 +0300

    add exception for not found param

---
+++ receipt-handler-service/src/main/java/app/components/exception/NotFoundParameterException.java
@@ -0,0 +1,8 @@
+package app.components.exception;
+
+public class NotFoundParameterException extends Exception {
+
+     public NotFoundParameterException(String message){
+         super(message);
+     }
+}
