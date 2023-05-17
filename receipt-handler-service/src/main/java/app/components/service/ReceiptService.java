8e20c25665bb4b5b4635e8810602b9f25cdfc105
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-17 17:38:38 +0300

    add logic

--- receipt-handler-service/src/main/java/app/components/service/ReceiptService.java
+++ receipt-handler-service/src/main/java/app/components/service/ReceiptService.java
@@ -1,5 +1,7 @@
 package app.components.service;
 
+import app.components.exception.NotFoundParameterException;
+import app.components.Status;
 import app.components.entity.Receipt;
 import app.components.repository.ReceiptsRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,15 +19,28 @@ public class ReceiptService {
         this.receiptsRepo = receiptsRepo;
     }
 
-    public List<Receipt> getAll(long idUser){
+    public List<Receipt> getAll(Long idUser) throws NotFoundParameterException {
+        if (idUser == null){
+            throw new NotFoundParameterException("No idUser");
+        }
         return receiptsRepo.getReceipts(idUser);
     }
 
-    public List<Receipt> getByDate(long idUser, LocalDate date){
+    public List<Receipt> getByDate(Long idUser, LocalDate date) throws NotFoundParameterException {
+        if (idUser == null || date == null){
+            throw new NotFoundParameterException("incorrect amount of data");
+        }
         return receiptsRepo.findReceiptsByDate(idUser, date);
     }
 
-    public void delete(long id){
-        receiptsRepo.deleteById(id);
+    public Status delete(Long id) throws NotFoundParameterException {
+        if (id == null){
+            throw new NotFoundParameterException("No id");
+        }
+        if (receiptsRepo.deleteById(id) == 1){
+            return Status.Success;
+        }
+        return Status.ServerError;
     }
+
 }
