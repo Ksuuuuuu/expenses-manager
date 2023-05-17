2bf287851910aa00715f513d8c2f66e5762b5c2f
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-17 17:36:11 +0300

    сделала не static

--- qr-read-service/src/main/java/app/components/service/ParseService.java
+++ qr-read-service/src/main/java/app/components/service/ParseService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 @Service
 public class ParseService {
-    public static ReceiptContent parseContent(String content){
+    public  ReceiptContent parseContent(String content){
         final String[] contentParts =  content.split("&");
         final String dateTimeString = contentParts[0].substring(2);
 
@@ -14,7 +14,7 @@ public class ParseService {
         return new ReceiptContent(getFormattedDateString(dateTimeString), amount);
     }
 
-    private static String getFormattedDateString(String dateTimeString){
+    private  String getFormattedDateString(String dateTimeString){
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(dateTimeString);
         stringBuilder.insert(4, '-');
