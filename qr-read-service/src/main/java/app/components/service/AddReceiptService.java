9856385e45e2c18ec46999c1d1a3d1749956bfe9
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-17 17:28:44 +0300

    add Status

--- qr-read-service/src/main/java/app/components/service/AddReceiptService.java
+++ qr-read-service/src/main/java/app/components/service/AddReceiptService.java
@@ -1,5 +1,6 @@
 package app.components.service;
 
+import app.components.Status;
 import app.components.entity.ReceiptContent;
 import app.components.entity.ReceiptEntity;
 import app.components.entity.ReceiptLoad;
@@ -13,20 +14,30 @@ import java.io.IOException;
 
 @Service
 public class AddReceiptService {
-    final AddReceiptRepository receiptRepository;
-    final QrReadService qrReadService;
+    private final AddReceiptRepository receiptRepository;
+    private final QrReadService qrReadService;
+    private final ParseService parseService;
 
     @Autowired
-    public AddReceiptService(AddReceiptRepository receiptRepository, QrReadService qrReadService) {
+    public AddReceiptService(AddReceiptRepository receiptRepository, QrReadService qrReadService, ParseService parseService) {
         this.receiptRepository = receiptRepository;
         this.qrReadService = qrReadService;
+        this.parseService = parseService;
     }
 
-    public void save(ReceiptLoad receiptLoad) throws FileNotFoundException {
+    public Status save(ReceiptLoad receiptLoad) throws FileNotFoundException {
         try {
             String path = receiptLoad.getFilePath();
-            ReceiptContent content = ParseService.parseContent(qrReadService.readQRCode(new File(path)));
-            receiptRepository.create(new ReceiptEntity(content.getDateTime(), content.getAmount(), path, receiptLoad.getIdUser()));
+            ReceiptContent content = parseService.parseContent(qrReadService.readQRCode(new File(path)));
+            Long id = receiptRepository.create(new ReceiptEntity(content.getDateTime(),
+                    content.getAmount(), path, receiptLoad.getIdUser()));
+            if (id != null){
+                return Status.Success;
+            }
+            else{
+                return Status.ServerError;
+            }
+
         } catch (IOException e) {
             throw new FileNotFoundException();
         }
