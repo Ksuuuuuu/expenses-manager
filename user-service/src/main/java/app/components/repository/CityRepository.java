c7acd1fc4a1c007fdd5f009800020f6b180b02e0
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-17 17:27:06 +0300

    change namedjdbc to jdbc

--- user-service/src/main/java/app/components/repository/CityRepository.java
+++ user-service/src/main/java/app/components/repository/CityRepository.java
@@ -3,45 +3,51 @@ package app.components.repository;
 import app.components.entity.City;
 import app.components.mapper.CityRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class CityRepository {
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-    private final static String GET_BY_NAME = "SELECT city_id, city_name FROM city WHERE city_name = :city_name";
+    private final static String GET_BY_NAME = "SELECT city_id, city_name FROM city WHERE city_name = ?";
     private final static String GET_ALL = "SELECT city_id, city_name FROM city";
-    private final static String INSERT = "INSERT INTO city VALUES(:city_name)";
-    private static final String GET_BY_ID = "SELECT city_id, city_name FROM city WHERE city_id = :city_id";
+    private final static String INSERT = "INSERT INTO city VALUES(?)";
+    private static final String GET_BY_ID = "SELECT city_id, city_name FROM city WHERE city_id = ?";
 
     @Autowired
-    public CityRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    public CityRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<City> getCities(){
-        return namedParameterJdbcTemplate.query(GET_ALL, new CityRowMapper());
+        return jdbcTemplate.query(GET_ALL, new CityRowMapper());
     }
 
     public Optional<City> findCityByName(String name){
-        SqlParameterSource param = new MapSqlParameterSource("city_name", name);
-        return namedParameterJdbcTemplate.query(GET_BY_NAME, new CityRowMapper()).stream().findFirst();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(GET_BY_NAME, new Object[]{name}, new CityRowMapper()));
     }
 
     public Optional<City> findCityById(long id){
-        SqlParameterSource param = new MapSqlParameterSource("city_id", id);
-        return namedParameterJdbcTemplate.query(GET_BY_ID, new CityRowMapper()).stream().findFirst();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(GET_BY_ID, new Object[]{id}, new CityRowMapper()));
     }
 
-    public void create(City city){
-        MapSqlParameterSource param = new MapSqlParameterSource("city_name", city.getTitle());
-        namedParameterJdbcTemplate.update(INSERT, param);
+    public Long create(City city){
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement pst = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+            pst.setString(1, city.getTitle());
+            return pst;
+        }, keyHolder);
+
+        return keyHolder.getKey() == null ? null : keyHolder.getKey().longValue();
     }
 }
--- user-service/src/main/java/app/components/repository/UserRepository.java
+++ user-service/src/main/java/app/components/repository/UserRepository.java
@@ -3,58 +3,56 @@ package app.components.repository;
 import app.components.entity.User;
 import app.components.mapper.UserRowMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Optional;
 
 @Repository
 public class UserRepository {
 
-    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private final static String GET_BY_LOGIN = "SELECT user_id, login, password, name, city_id FROM \"user\" WHERE login = :login";
-    private final static String INSERT = "INSERT INTO \"user\" VALUES(:login, :password, :name, :city_id)";
-    private static final String GET_BY_ID = "SELECT user_id, login, password, name, city_id FROM \"user\" WHERE user_id = :user_id";
-    private final static String DELETE_BY_ID = "DELETE FROM \"user\" WHERE login = :login";
-    private final static String UPDATE = "UPDATE \"user\" SET login = :login, password = :password, name = :name, city_id = :city_id  WHERE user_id = :user_id";
+    private final JdbcTemplate jdbcTemplate;
+    private final static String GET_BY_LOGIN = "SELECT user_id, login, password, name, city_id FROM \"user\" WHERE login = ?";
+    private final static String INSERT = "INSERT INTO \"user\" VALUES(?, ?, ?, ?)";
+    private static final String GET_BY_ID = "SELECT user_id, login, password, name, city_id FROM \"user\" WHERE user_id = ?";
+    private final static String DELETE_BY_ID = "DELETE FROM \"user\" WHERE login = ?";
+    private final static String UPDATE = "UPDATE \"user\" SET login = ?, password = ?, name = ?, city_id = ?  WHERE user_id = ?";
     @Autowired
-    public UserRepository(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
-        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public Optional<User> findUserByLogin(String login) {
-        SqlParameterSource param = new MapSqlParameterSource("city_name", login);
-        return namedParameterJdbcTemplate.query(GET_BY_LOGIN, param, new UserRowMapper()).stream().findFirst();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(GET_BY_LOGIN, new Object[]{login}, new UserRowMapper()));
     }
 
     public Optional<User> findUserById(long id) {
-        SqlParameterSource param = new MapSqlParameterSource("city_id", id);
-        return namedParameterJdbcTemplate.query(GET_BY_ID, param, new UserRowMapper()).stream().findFirst();
+        return Optional.ofNullable(jdbcTemplate.queryForObject(GET_BY_ID, new Object[]{id}, new UserRowMapper()));
     }
 
-    public int create(User user) {
-        MapSqlParameterSource param = new MapSqlParameterSource();
-        param.addValue("login", user.getLogin());
-        param.addValue("password", user.getPassword());
-        param.addValue("name", user.getName());
-        param.addValue("city_id", user.getIdCity());
-        return namedParameterJdbcTemplate.update(INSERT, param);
+    public Long create(User user) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement pst = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
+            pst.setString(1, user.getLogin());
+            pst.setString(2, user.getPassword());
+            pst.setString(3, user.getName());
+            pst.setLong(4, user.getIdCity());
+            return pst;
+        }, keyHolder);
+
+        return keyHolder.getKey() == null ? null : keyHolder.getKey().longValue();
     }
 
     public int deleteById(long id) {
-        SqlParameterSource param = new MapSqlParameterSource("city_id", id);
-        return namedParameterJdbcTemplate.update(DELETE_BY_ID, param);
+        return jdbcTemplate.update(DELETE_BY_ID, id);
     }
 
     public int update(User user) {
-        MapSqlParameterSource param = new MapSqlParameterSource();
-        param.addValue("user_id", user.getId());
-        param.addValue("login", user.getLogin());
-        param.addValue("password", user.getPassword());
-        param.addValue("name", user.getName());
-        param.addValue("city_id", user.getIdCity());
-        return namedParameterJdbcTemplate.update(UPDATE, param);
+        return jdbcTemplate.update(UPDATE, user.getId(), user.getLogin(), user.getPassword(), user.getName(), user.getIdCity());
     }
 }
