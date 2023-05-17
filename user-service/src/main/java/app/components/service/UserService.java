a15b8db007c9ff6755f171fecc3ff3087804e13c
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-17 17:40:38 +0300

    add logic

--- user-service/src/main/java/app/components/service/UserService.java
+++ user-service/src/main/java/app/components/service/UserService.java
@@ -1,48 +1,56 @@
 package app.components.service;
 
 import app.components.Status;
+import app.components.entity.City;
+import app.components.entity.LoginUser;
+import app.components.entity.RegistryUser;
 import app.components.entity.User;
+import app.components.exception.IncorrectDataException;
+import app.components.repository.CityRepository;
 import app.components.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final CityRepository cityRepository;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, CityRepository cityRepository) {
         this.userRepository = userRepository;
+        this.cityRepository = cityRepository;
     }
 
-    public Status registry(User user){
-        if (userRepository.create(user) == 1){
+    public Long registry(RegistryUser user) {
+        City city = cityRepository.findCityByName(user.getCity()).orElse(null);
+        Long idCity;
+        if (city == null) {
+            idCity = cityRepository.create(new City(user.getCity()));
+        } else {
+            idCity = city.getId();
+        }
+        return userRepository.create(new User(user.getLogin(), user.getPassword(), user.getName(), idCity));
+    }
+
+    public Long login(LoginUser loginUser) throws IncorrectDataException {
+        User user = userRepository.findUserByLogin(loginUser.getLogin()).orElse(null);
+        if (user == null || !user.getPassword().equals(loginUser.getPassword())) {
+            throw new IncorrectDataException("incorrect data");
+        }
+        return user.getId();
+    }
+
+    public Status update(User user) {
+        if (userRepository.update(user) == 1) {
             return Status.Success;
         }
         return Status.ServerError;
     }
 
-    public Status login(String login, String password){
-        Optional<User> user = userRepository.findUserByLogin(login);
-        if (user.isPresent()) {
-            if (user.get().getPassword().equals(password))
-                return Status.Success;
-        }
-        return Status.ClientError;
-    }
-
-    public Status update(User user){
-        if (userRepository.update(user) == 1){
-            return Status.Success;
-        }
-        return Status.ServerError;
-    }
-
-    public Status delete(long id){
-        if (userRepository.deleteById(id) == 1){
+    public Status delete(long id) {
+        if (userRepository.deleteById(id) == 1) {
             return Status.Success;
         }
         return Status.ServerError;
