be88b57c7d385f8f793c8033451473a2ae724961
Ксения Канищева <ksenia.kanishewa@yandex.ru>
2023-05-18 22:29:39 +0300

    add constructor

--- web-read-service/src/main/java/app/components/entity/ReceiptLoad.java
+++ web-read-service/src/main/java/app/components/entity/ReceiptLoad.java
@@ -6,4 +6,8 @@ import lombok.Data;
 public class ReceiptLoad {
     long idUser;
     String filePath;
+    public ReceiptLoad(String path, Long idUser) {
+        this.filePath = path;
+        this.idUser = idUser;
+    }
 }
