774a69f615321d4b9c34687ad5140c21cba9a317
Ksuuuuuu <ksenia.kanishewa@yandex.ru>
2023-11-28 21:08:15 +0300

    rename class

--- web-read-service/src/main/java/app/components/AppResponse.java
+++ web-read-service/src/main/java/app/components/AppResponse.java
@@ -3,11 +3,11 @@ package app.components;
 import lombok.Data;
 
 @Data
-public class AppError {
+public class AppResponse {
     private int statusCode;
     private String message;
 
-    public AppError(int statusCode, String message) {
+    public AppResponse(int statusCode, String message) {
         this.statusCode = statusCode;
         this.message = message;
     }
