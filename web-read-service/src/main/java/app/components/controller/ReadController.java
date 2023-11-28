f4fc90a7d87d3df22ec881668559282dae99e8c9
Ksuuuuuu <ksenia.kanishewa@yandex.ru>
2023-11-28 21:15:24 +0300

    delete saving file

--- web-read-service/src/main/java/app/components/controller/ReadController.java
+++ web-read-service/src/main/java/app/components/controller/ReadController.java
@@ -1,22 +1,25 @@
 package app.components.controller;
 
-import app.components.AppError;
+import app.components.AppResponse;
 import app.components.entity.ReceiptLoad;
 import app.components.service.SaveFileService;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/receipts/add")
 public class ReadController {
-
+    Logger logger = LoggerFactory.getLogger(ReadController.class);
     private final static String QUEUE_NAME = "message-queue";
     private final SaveFileService saveFileService;
     private final AmqpTemplate template;
@@ -28,18 +31,13 @@ public class ReadController {
     }
 
     @PostMapping("/{idUser}")
-    public ResponseEntity<?> readReceipt(@RequestBody File file, @PathVariable Long idUser) {
-        try {
-            //save file to s3
-            //path + idUser -> receipt load
-            String path = saveFileService.saveFileToDir(file);
-            ReceiptLoad receiptLoad = new ReceiptLoad(path, idUser);
-            //write to queue with receipt load
-            template.convertAndSend(QUEUE_NAME, new Gson().toJson(receiptLoad));
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (FileNotFoundException e) {
-            return new ResponseEntity<>(new AppError(HttpStatus.NOT_FOUND.value(), "File doesn't found"), HttpStatus.NOT_FOUND);
-        }
-
+    public ResponseEntity<?> readReceipt(@RequestBody String filePath, @PathVariable Long idUser) {
+        System.out.println("Загрузили файл " + filePath);
+        //String path = saveFileService.saveFileToDir(new File(fileName));
+        ReceiptLoad receiptLoad = new ReceiptLoad(filePath, idUser);
+        //write to queue with receipt load
+        System.out.println("Формируем и отправляем сообщение");
+        template.convertAndSend(QUEUE_NAME, new Gson().toJson(receiptLoad));
+        return ResponseEntity.ok("success");
     }
 }
