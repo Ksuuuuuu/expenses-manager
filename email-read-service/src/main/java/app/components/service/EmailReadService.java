08418658cb8569be9d6d5416d048788190a27d71
Ksuuuuuu <ksenia.kanishewa@yandex.ru>
2024-02-13 20:31:47 +0300

    try read email

--- email-read-service/src/main/java/app/components/service/EmailReadService.java
+++ email-read-service/src/main/java/app/components/service/EmailReadService.java
@@ -2,8 +2,11 @@ package app.components.service;
 
 import org.springframework.stereotype.Service;
 
-import javax.mail.*;
-import java.io.FileInputStream;
+
+import javax.mail.Folder;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Store;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -18,31 +21,18 @@ public class EmailReadService {
             try (InputStream is = getClass().getClassLoader().getResourceAsStream("configEmail.properties")) {
                 authProperties.load(is);
             }
-            try (InputStream is = getClass().getClassLoader().getResourceAsStream("sessionEmail.properties")) {
-                sessionProperties.load(is);
-            }
 
+            sessionProperties.put("mail.store.protocol", "imap");
+            sessionProperties.put("mail.imap.ssl.enable", "true");
+            sessionProperties.put("mail.imaps.port", 993);
+            sessionProperties.put("mail.imaps.sasl.mechanisms", "XOAUTH2");
 
-            Session session = Session.getDefaultInstance(sessionProperties, new Authenticator() {
-                @Override
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(
-                            (String) authProperties.get("email"),
-                            (String) authProperties.get("pwd")
-                    );
-                }
-            });
-
-           // Session session = Session.getDefaultInstance(sessionProperties);
-
-            Store store = session.getStore();
-
-            store.connect(authProperties.getProperty("imap.host"), authProperties.getProperty("email"),  authProperties.getProperty("pwd"));
-
+            //sessionProperties.put("mail.smtp.ssl.enable", "true");
+            Store store = Session.getInstance(sessionProperties).getStore();
+            store.connect(authProperties.getProperty("email.host"), authProperties.getProperty("email"),  authProperties.getProperty("email.pwd"));
             Folder inbox = store.getFolder("INBOX");
-
             inbox.open(Folder.READ_ONLY);
-            System.out.println(inbox.getMessageCount());
+            System.out.println("Count Message -- "+ inbox.getMessageCount());
 
 //            Message message = inbox.getMessage(inbox.getMessageCount());
 //
